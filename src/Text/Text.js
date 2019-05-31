f343c9c3d27cdeb451e6fa772a12120190e76339
Alex Buchan <alex.buchan@sage.com>
2019-05-31 11:05:22 +0200

    Initial commit

---
+++ src/Text/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Text extends React.Component {
+
+  render() {
+    return (
+      <div>
+      This is only a test
+      </div>
+    );
+  }
+}
+
+export default Text;
